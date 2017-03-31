cacbb44c409f418999b35ce586bcab035caac879
Peter Wagenet <peter.wagenet@gmail.com>
2017-03-31 13:52:13 -0700

    Add some comments

--- spec/panic_test_spec.rb
+++ spec/panic_test_spec.rb
@@ -3,6 +3,7 @@ require 'panic_test'
 describe "PanicTest" do
 
   it "panics" do
+    # This is calling the panic method we defined in src/lib.rs
     expect(Object.new.panic).to be_nil
     expect(Object.new.panic).to be_nil
   end
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate libcruby_sys as sys;
 
 use std::ffi::CString;
 
+// This will become Object#panic in Ruby
 #[no_mangle]
 pub extern "C" fn panic() -> sys::VALUE {
     let res = ::std::panic::catch_unwind(|| {
@@ -14,10 +15,12 @@ pub extern "C" fn panic() -> sys::VALUE {
     unsafe { sys::Qnil }
 }
 
+// This method is called when Ruby loads the native extension
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn Init_native() {
     unsafe {
+        // This is a built-in Ruby method that allows us to define C methods on Ruby object
         sys::rb_define_method(sys::rb_cObject, CString::new("panic").unwrap().as_ptr(), panic as *const libc::c_void, 0);
     }
 }
