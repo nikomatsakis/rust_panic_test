45c31e00e31771e43406d74bc31aa5a2d7375470
Peter Wagenet <peter.wagenet@gmail.com>
2017-04-03 17:18:15 -0700

    Remove libcruby-sys dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,44 @@
+#![allow(non_camel_case_types)]
+
 extern crate libc;
-extern crate libcruby_sys as sys;
 
 use std::ffi::CString;
 
+pub type void_ptr = *const libc::c_void;
+pub type c_string = *const libc::c_char;
+
+#[repr(C)]
+#[derive(Eq, PartialEq, Copy, Clone, Debug)]
+pub struct VALUE(void_ptr);
+
+extern "C" {
+    #[link_name = "HELIX_Qnil"]
+    pub static Qnil: VALUE;
+
+    #[link_name = "rb_eRuntimeError"]
+    pub static rb_eRuntimeError: VALUE;
+
+    #[link_name = "rb_cObject"]
+    pub static rb_cObject: VALUE;
+
+    pub fn rb_raise(exc: VALUE, string: c_string, ...) -> !;
+    pub fn rb_define_method(class: VALUE, name: c_string, func: void_ptr, arity: isize);
+}
+
 // This will become Object#panic in Ruby
 #[no_mangle]
-pub extern "C" fn panic() -> sys::VALUE {
+pub extern "C" fn panic() -> VALUE {
     let _ = ::std::panic::catch_unwind(|| {
         panic!("Panic!")
     });
-    unsafe { sys::Qnil }
+    unsafe { Qnil }
 }
 
 // This will become Object#rust_raise in Ruby
 #[no_mangle]
 pub extern "C" fn rust_raise() -> ! {
     unsafe {
-        sys::rb_raise(sys::rb_eRuntimeError, CString::new("Panicked in Rust").unwrap().as_ptr())
+        rb_raise(rb_eRuntimeError, CString::new("Panicked in Rust").unwrap().as_ptr())
     }
 }
 
@@ -34,8 +56,8 @@ pub extern "C" fn panic_and_raise() -> ! {
 pub extern "C" fn Init_native() {
     unsafe {
         // This is a built-in Ruby method that allows us to define C methods on Ruby object
-        sys::rb_define_method(sys::rb_cObject, CString::new("panic").unwrap().as_ptr(), panic as *const libc::c_void, 0);
-        sys::rb_define_method(sys::rb_cObject, CString::new("rust_raise").unwrap().as_ptr(), rust_raise as *const libc::c_void, 0);
-        sys::rb_define_method(sys::rb_cObject, CString::new("panic_and_raise").unwrap().as_ptr(), panic_and_raise as *const libc::c_void, 0);
+        rb_define_method(rb_cObject, CString::new("panic").unwrap().as_ptr(), panic as *const libc::c_void, 0);
+        rb_define_method(rb_cObject, CString::new("rust_raise").unwrap().as_ptr(), rust_raise as *const libc::c_void, 0);
+        rb_define_method(rb_cObject, CString::new("panic_and_raise").unwrap().as_ptr(), panic_and_raise as *const libc::c_void, 0);
     }
 }
