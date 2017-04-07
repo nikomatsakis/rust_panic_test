29c2b6df2fd25b69d35cdf2d11292c500da2b358
Niko Matsakis <niko@alum.mit.edu>
2017-04-07 05:37:00 -0400

    Revert "test if call to rust_raise is imp't"
    This reverts commit 0d3bfcd1fc30dc1bd860fb33fd5ccfde89ebf5af.

--- spec/panic_test_spec.rb
+++ spec/panic_test_spec.rb
@@ -5,7 +5,7 @@ describe "PanicTest" do
   it "panics" do
     # This is calling the methods we defined in src/lib.rs
     puts "BEFORE RAISE"
-    # expect { Object.new.rust_raise }.to raise_error(RuntimeError, "Panicked in Rust")
+    expect { Object.new.rust_raise }.to raise_error(RuntimeError, "Panicked in Rust")
     puts "AFTER RAISE"
     expect(Object.new.panic).to be_nil
   end
